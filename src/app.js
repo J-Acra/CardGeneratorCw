6f0becb4cd37e928a8b07fd59e1227ecdd8c4924
Jonathan Acra <jonathanacra@gmail.com>
2021-12-09 03:42:34 +0000

    first save, progress towards blackjack,deck generation and shuffle functions working.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,61 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let buttonPlaceHolder1 = document.querySelector("#genButton");
+  buttonPlaceHolder1.addEventListener("click", generateDeck);
+  let buttonPlaceHolder2 = document.querySelector("#shuffleButton");
+  buttonPlaceHolder2.addEventListener("click", shuffleBuild);
+
+  var cards = [];
+  var shuffledDeck = [];
+  var deckOfCards = [];
+
+  function shuffleBuild() {
+    var a = cards.length,
+      b,
+      c;
+    while (a) {
+      // Pick a remaining element…
+      c = Math.floor(Math.random() * a--);
+      // And swap it with the current element.
+      b = cards[a];
+      cards[a] = cards[c];
+      cards[c] = b;
+    }
+    shuffledDeck = cards;
+    console.log(shuffledDeck);
+  }
+  function generateDeck() {
+    var cardSuits = ["spade", "club", "heart", "diamond"];
+    var cardNumbers = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "jack",
+      "queen",
+      "king"
+    ];
+    if (deckOfCards.length == 0) {
+      for (var a = 0; a < cardSuits.length; a++) {
+        for (var b = 0; b < cardNumbers.length; b++) {
+          const cardSuitsX = cardSuits[a];
+          const cardNumbersX = cardNumbers[b];
+          deckOfCards.push({ cardSuitsX, cardNumbersX });
+        }
+      }
+      cards = deckOfCards;
+      console.log(cards);
+    }
+  }
+
+  function getCardSuit() {}
+  function getCardNumber() {}
 };

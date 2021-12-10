da0defe433a0f245643cf994bda62366d98d0cf5
Jonathan Acra <jonathanacra@gmail.com>
2021-12-10 21:28:36 +0000

    saving changes

--- src/app.js
+++ src/app.js
@@ -7,13 +7,15 @@ window.onload = function() {
   buttonPlaceHolder1.addEventListener("click", generateDeck);
   let buttonPlaceHolder2 = document.querySelector("#shuffleButton");
   buttonPlaceHolder2.addEventListener("click", shuffleBuild);
-  //let buttonPlaceHolder3 = document.querySelector("#hitCardButton");
-  //buttonPlaceHolder3.addEventListener("click", hitCard);
+  let buttonPlaceHolder3 = document.querySelector("#generateHand");
+  buttonPlaceHolder3.addEventListener("click", dealHands);
   //let buttonPlaceHolder4 = document.querySelector("#standCardButton");
   //buttonPlaceHolder4.addEventListener("click", standCard);
+  //let buttonPlaceHolder5 = document.querySelector("#standCardButton");
+  //buttonPlaceHolder5.addEventListener("click", standCard);
 
-  var playerHand = [];
-  var dealerHand = [];
+  let playerHand = [];
+  let dealerHand = [];
   var backAccount = [];
   var cards = [];
   var shuffledDeck = [];
@@ -33,9 +35,9 @@ window.onload = function() {
     console.log(shuffledDeck);
   }
   function generateDeck() {
-    var cardSuits = ["spade", "club", "heart", "diamond"];
+    var cardSuits = ["spades", "clubs", "hearts", "diams"];
     var cardNumbers = [
-      "1",
+      "A",
       "2",
       "3",
       "4",
@@ -45,9 +47,9 @@ window.onload = function() {
       "8",
       "9",
       "10",
-      "jack",
-      "queen",
-      "king"
+      "J",
+      "Q",
+      "K"
     ];
     if (deckOfCards.length == 0) {
       for (var a = 0; a < cardSuits.length; a++) {
@@ -61,5 +63,45 @@ window.onload = function() {
       console.log(cards);
     }
   }
-  function hitCard() {}
+  function updatePlayerCard1() {
+    document.querySelector(".playerCard1Suit").innerHTML =
+      "&" + playerHand[0].cardSuitsX + ";";
+    document.querySelector(".playerCard1Rank").innerHTML =
+      playerHand[0].cardNumbersX;
+  }
+  function updatePlayerCard2() {
+    document.querySelector(".playerCard2Suit").innerHTML =
+      "&" + playerHand[1].cardSuitsX + ";";
+    document.querySelector(".playerCard2Rank").innerHTML =
+      playerHand[1].cardNumbersX;
+  }
+  function updateDealerCard1() {
+    document.querySelector(".dealerCard1Suit").innerHTML =
+      "&" + dealerHand[0].cardSuitsX + ";";
+    document.querySelector(".dealerCard1Rank").innerHTML =
+      dealerHand[0].cardNumbersX;
+  }
+  function updateDealerCard2() {
+    document.querySelector(".dealerCard2Suit").innerHTML =
+      "&" + dealerHand[1].cardSuitsX + ";";
+    document.querySelector(".dealerCard2Rank").innerHTML =
+      dealerHand[1].cardNumbersX;
+  }
+  function dealHands() {
+    while (playerHand.length < 2) {
+      var x = shuffledDeck.pop();
+      playerHand.push(x);
+    }
+    while (dealerHand.length < 2) {
+      var x = shuffledDeck.pop();
+      dealerHand.push(x);
+    }
+    updateDealerCard1();
+    updateDealerCard2();
+    updatePlayerCard1();
+    updatePlayerCard2();
+    console.log(playerHand);
+    console.log(dealerHand);
+    console.log(shuffledDeck);
+  }
 };
